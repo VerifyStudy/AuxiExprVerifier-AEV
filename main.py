7a06d5fa8fdb7db1485a3d20ef3cb43072d80fb7
LiangHaowei <39672402+liangliangboy@users.noreply.github.com>
2021-11-01 18:41:15 +0800

    modify path

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from passModule.PassCompleteBlock import PassCompleteBlock
 from passModule.PassAuxiExpr import PassAuxiExpr
 
 
-orgDir = "/home/liangle/verifier/CFG-dupath-of-C/"
-preDir = "/home/liangle/verifier/CFG-dupath-of-C/tmp/"
-tmpDir = "/home/liangle/verifier/CFG-dupath-of-C/tmp/"
+orgDir = "/home/liangle/verifier/AuxiExprVerifier-AEV/"
+preDir = "/home/liangle/verifier/AuxiExprVerifier-AEV/tmp/"
+tmpDir = "/home/liangle/verifier/AuxiExprVerifier-AEV/tmp/"
 file = "test.c"
 PM = PassManager(orgDir, preDir, tmpDir, file)
 PM.add(PassPyParse)
